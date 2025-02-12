8a8ddd8c3d129944736d0dc3e39b99aec87e95f6
Brette Fitzgibbon <79230918+BretteConnolly@users.noreply.github.com>
2025-02-12 12:30:58 -0800

    Update lab

--- src/main/java/linkedlists/LinkedList.java
+++ src/main/java/linkedlists/LinkedList.java
@@ -1,4 +1,5 @@
 package linkedlists;
+import java.util.Iterator;
 
 /** A class representing a singly linked list. */
 public class LinkedList {
@@ -16,7 +17,11 @@ public class LinkedList {
 	 */
 	public void append(int elem) {
 		Node newNode = new Node(elem);
-		// FILL IN CODE
+		if (head == null) {head = tail = newNode;}
+		else {
+			tail.setNext(newNode);
+			tail = newNode;
+		}
 	}
 
 	/**
@@ -24,17 +29,22 @@ public class LinkedList {
 	 * @param elem element
 	 */
 	public void insertAtFront(int elem) {
-		// FILL IN CODE:
-		// Insert a new node with elem in front of the linked list
-
+		Node newNode = new Node(elem);
+		if (head == null) {
+			head = tail = newNode;
+		}
+		else {
+			newNode.setNext(head);
+			head = newNode;
+		}
 	}
 
 
-	/** Prints all the nodes in the link list */
+	/** Prints all the nodes in the linked list */
 	public void printNodes() {
 		Node current = head;
 		while (current != null) {
-			System.out.print(current.getElem() + " ");
+			System.out.print(current.getElem() + "->");
 			current = current.next();
 		}
 		System.out.println();
@@ -46,12 +56,24 @@ public class LinkedList {
 	 * @param elem element
 	 */
 	public void insertAtIndex(int i, int elem) {
-		// FILL IN CODE: insert a new node with the given elem
-		// at "index" i
-		// Example:
-		// If the linked list is 5 -> 7 - > 2 and we call
-		// insertAtIndex(1, 8), the linked list will be
-		// 5 -> 8 -> 7 -> 2  we inserted element 8 at "index" = 1.
+		if (head == null) {System.out.println("Empty list");}
+		else if (i == 0) {insertAtFront(elem);}
+		else {
+			Node newNode = new Node(elem);
+			Node current = head;
+			int count = 0;
+			while (current != null && count < i - 1){
+				current = current.next();
+				count++;
+			}
+			if (current == null) {System.out.println("Invalid index");}
+			else {
+				newNode.setNext(current.next());
+				current.setNext(newNode);
+				if (current == tail) {tail = newNode;}
+			}
+		}
+
 	}
 
 	/**
@@ -59,11 +81,13 @@ public class LinkedList {
 	 * @return reference to the middle node
 	 */
 	public Node getMiddleNode() {
-	 // FILL IN CODE: use slow and fast points to return
-	 // the node in the middle of the linked list in one pass
-
-	 return null;
-
+		Node slow = head;
+		Node fast = head;
+		while (fast.next().next() != null) {
+			slow = slow.next();
+			fast = fast.next().next();
+		}
+		return slow;
 	}
 
 	/**
@@ -71,6 +95,54 @@ public class LinkedList {
 	 * @param elem element
 	 */
 	public void removeNode(int elem) {
-		// FILL IN CODE: remove the first node it finds with the given element
+		Node current = head;
+		while (current.next().getElem() != elem) {current = current.next();}
+		current.setNext(current.next().next());
+	}
+
+	public Node head() {return head;}
+
+	public Node tail() {return tail;}
+
+	public Node getKthFromEnd(int k) {
+		if (head == null) {
+			System.out.println("Empty list");
+			return null;
+		}
+		Node fast = head;
+		Node slow = head;
+		int count = 0;
+		while(fast != null && count < k){
+			fast = fast.next();
+			count++;
+		}
+		if (fast == null) {
+			System.out.println("Index is invalid");
+			return null;
+		}
+		while (fast != tail) {
+			fast = fast.next();
+			slow = slow.next();
+		}
+		return slow;
+	}
+
+	public Iterator <Node> iterator () {return new MyLinkedListIterator();}
+
+	public class MyLinkedListIterator implements Iterator <Node> {
+		private Node curr;
+
+		public MyLinkedListIterator() {curr = head;}
+
+		public boolean hasNext() {return curr != null;}
+
+		public Node next() {
+			Node temp = curr;
+			if (hasNext()) {
+				curr = curr.next();
+				return curr;
+			}
+			return null;
+		}
 	}
 }
